74de30c036197c0601ff25440b3fd651385c7432
Jagdish Aneshwar <aneshwarjagdish@gmail.com>
2023-10-29 20:25:54 +0530

    update

---
+++ server/conn.js
@@ -0,0 +1,17 @@
+const mongo = require("mongoose");
+
+
+const mongoURI = process.env.MOONGODB_URL;
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connToMongo = async () => {
+  await mongo.connect(mongoURI, connectionParams).then(() => {
+    console.log("Successfully! connected with database");
+  });
+};
+
+module.exports = connToMongo;
