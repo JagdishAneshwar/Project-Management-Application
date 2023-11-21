4c16b1b88427e9d012e43ee3d4f4e644b72e12a2
Jagdish Aneshwar <aneshwarjagdish@gmail.com>
2023-11-21 10:08:24 +0530

    updated

--- frontend/src/pages/tabs/employee/Employee.js
+++ frontend/src/pages/tabs/employee/Employee.js
@@ -1,4 +1,5 @@
 import React,{useState, useRef} from "react";
+import Select from 'react-select'
 import EmployeeCard from "../../../features/component/employee-card/EmployeeCard";
 
 import "./_employee.scss";
@@ -6,6 +7,11 @@ import "./_employee.scss";
 const Employee = () => {
   const refClose = useRef();
   const ref = useRef(null);
+  const  [ selectedValueGender, setSelectedValueGender] = useState(null)
+  const [selectedValueRole, setSelectedValueRole] = useState([]);
+  const handleChangeRole = (e) => {
+    setSelectedValueRole(Array.isArray(e) ? e.map(x => x.value) : []);
+  }
   const [employees, setemployees] = useState({
     name: "", 
     age:"", 
@@ -24,7 +30,7 @@ const Employee = () => {
     
   }
 
-  const data = [
+  const gender = [
     {
       value: 1,
       label: "Male"
@@ -37,9 +43,47 @@ const Employee = () => {
     {value:4, label: 'Prefer not to say'}
 ]
 
-  const handleChange = (e) => {
-    setSelectedValue(Array.isArray(e) ? e.map(x => x.value) : []);
-  }
+const roles = [
+  { value: 1, label: 'Operations Manager' },
+  { value: 2, label: 'Operations Coordinator' },
+  { value: 3, label: 'Administrative Assistant' },
+  { value: 4, label: 'Accountant' },
+  { value: 5, label: 'Financial Analyst' },
+  { value: 6, label: 'Bookkeeper' },
+  { value: 7, label: 'HR Manager' },
+  { value: 8, label: 'HR Specialist' },
+  { value: 9, label: 'Recruiter' },
+  { value: 10, label: 'Marketing Manager' },
+  { value: 11, label: 'Marketing Coordinator' },
+  { value: 12, label: 'Content Writer' },
+  { value: 13, label: 'Sales Manager' },
+  { value: 14, label: 'Sales Representative' },
+  { value: 15, label: 'Account Manager' },
+  { value: 16, label: 'Customer Service Representative' },
+  { value: 17, label: 'Customer Support Specialist' },
+  { value: 18, label: 'Customer Success Manager' },
+  { value: 19, label: 'IT Manager' },
+  { value: 20, label: 'Systems Administrator' },
+  { value: 21, label: 'Software Developer' },
+  { value: 22, label: 'Research Scientist' },
+  { value: 23, label: 'R&D Engineer' },
+  { value: 24, label: 'Product Development Specialist' },
+  { value: 25, label: 'Production Supervisor' },
+  { value: 26, label: 'Quality Control Inspector' },
+  { value: 27, label: 'Assembly Line Worker' },
+  { value: 28, label: 'Supply Chain Manager' },
+  { value: 29, label: 'Logistics Coordinator' },
+  { value: 30, label: 'Warehouse Manager' },
+  { value: 31, label: 'Legal Counsel' },
+  { value: 32, label: 'Paralegal' },
+  { value: 33, label: 'Contracts Administrator' },
+  { value: 34, label: 'PR Manager' },
+  { value: 35, label: 'PR Specialist' },
+  { value: 36, label: 'Media Relations Coordinator' }]
+
+  const handleChange = (selectedOption) => {
+    setSelectedValueGender(selectedOption);
+  };
 
   const onChange = (e)=>{
     setemployees({...employees, [e.target.name]: e.target.value})
@@ -49,8 +93,8 @@ const Employee = () => {
       <h1>Employees</h1>
       <button type="button" class="btn btn-primary add-employee" data-bs-toggle="modal" data-bs-target="#addEmployee">Add New Employee</button>
       <div class="employee-modal modal fade" id="addEmployee" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
+  <div class="modal-dialog ">
+    <div class="modal-content employee-dialog">
       <div class="modal-header border-0">
         <h5 class="modal-title">Add Employee</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -86,11 +130,25 @@ const Employee = () => {
               Gender
             </label>
             <Select className='dropdown text-dark'
-            value={data.filter(obj => selectedValue.includes(obj.value))}
-            options={data}
+            value={selectedValueGender}
+            options={gender}
             onChange={handleChange}  
+            isSearchable
+            name="gender"
+            placeholder="Select Option"
+             />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Role
+            </label>
+            <Select className='dropdown text-dark'
+            value={roles.filter(obj => selectedValueRole.includes(obj.value))}
+            options={roles}
+            onChange={handleChangeRole}  
             isMulti
-            isClearable
+            isSearchable
+            name="role"
             placeholder="Select Option"
              />
           </div>
